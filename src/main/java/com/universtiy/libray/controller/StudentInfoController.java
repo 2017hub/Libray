0e0a46a7b372d974b3c1bdfa4911a266feb05971
longyaoyuan <longyaoyuan1987@163.com>
2018-06-14 18:47:41 +0800

    Springboot添加了日志配置

--- src/main/java/com/universtiy/libray/controller/StudentInfoController.java
+++ src/main/java/com/universtiy/libray/controller/StudentInfoController.java
@@ -2,6 +2,8 @@ package com.universtiy.libray.controller;
 
 import com.universtiy.libray.domain.StudentInfo;
 import com.universtiy.libray.service.StudentInfoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,13 +11,19 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class StudentInfoController {
+
     @Autowired
     StudentInfoService studentInfoService;
+
+    final Logger logger=LoggerFactory.getLogger(this.getClass());
+
 
     @RequestMapping("/stuInfo")
     @ResponseBody
     public String getStudentInfo(String id){
-        System.out.println("-----s----->"+id);
+        logger.info("----------->studentInfoController"+id);
+        //System.out.println("-----s----->"+id);
+
         StudentInfo studentInfo1 =studentInfoService.selectByPrimaryKey(id);
         System.out.println("-----s----->"+studentInfo1.getStName());
         return "StudentInfoController";
--- src/test/java/com/universtiy/libray/LibrayApplicationTests.java
+++ src/test/java/com/universtiy/libray/LibrayApplicationTests.java
@@ -2,15 +2,21 @@ package com.universtiy.libray;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class LibrayApplicationTests {
+    final Logger logger=LoggerFactory.getLogger(this.getClass());
 
     @Test
     public void contextLoads() {
+        logger.debug("------->debug");
     }
 
 }
